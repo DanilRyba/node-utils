9fa9dbd148d42dfb1a3e83de87a6b58184c74311
Danil Rybalchenko <danil.ryba01@gmail.com>
2023-10-09 16:16:10 +0300

    Release 0.0.2

--- src/types/events.ts
+++ src/types/events.ts
@@ -2,7 +2,7 @@ export interface IEvent<T = unknown> {
     on(handler: EventHandler): (() => void) | null;
     off(handler: EventHandler): void;
 
-    trigger(data?: T): void;
+    trigger(data?: T): Promise<Error[]>;
 }
 
 export type EventHandler<T = unknown> = (data?: T) => void | Promise<void>;
